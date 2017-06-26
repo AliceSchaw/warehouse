775715a104d810932b54b47d502667722bc8eaad
Alice <schawxiao@foxmail.com>
2017-06-26 17:31:12 +0800

    change device_list import/export button

--- tmp/compile/device_list.htm.php
+++ tmp/compile/device_list.htm.php
@@ -6,12 +6,17 @@
 		  <div class="page-header">
             <h3><?php echo $this->_var['title']; ?></h3>
           </div>
-
+	<form action="?action=import" method="post" class="form-search">
+    	<input type="file" name="fileUpload"/>
+    	<input type="submit" value="表格导入" class="btn"/>
+    	<a href="?action=export" class="btn"> 数据导出</a>
+    	<a href="?action=address&do=returnlate" class="btn btn-primary pull-right"><i class="icon-th-list icon-white"></i> 超期 </a>
+    </form>
 	<form class="form-search" action="" method="post" >
 		<?php if ($_SESSION['roleid'] == "1"): ?>
 		<a href="?action=address&do=new" class="btn btn-primary pull-right"><i class="icon-pencil icon-th-list icon-white"></i> 新建</a>
 
-		<a href="?action=address&do=returnlate" class="btn btn-primary pull-right"><i class="icon-th-list icon-white"></i> 超期 </a>
+
 		<?php else: ?>
 
 		<?php endif; ?>
